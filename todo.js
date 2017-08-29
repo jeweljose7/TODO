b5f4ddf234378a4880b6186d7526dbcd3fd13f90
jewel <jewel@qburst.com>
2017-08-29 18:12:34 +0530

    end of the day

--- todo.js
+++ todo.js
@@ -1,64 +1,49 @@
-function get_todos() {
-    var todos = new Array;
-    var todos_str = localStorage.getItem('todo');
-    if (todos_str != null) {
-        todos = JSON.parse(todos_str); 
-    }
-    return todos;
+
+var counter=0;
+
+function drag_handler(ev) {
+	console.log("Drag");
+}
+function dragstart_handler(ev) {
+	console.log("dragStart");
+	ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop_handler(ev,form) {
+	console.log("Drop");
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	form.appendChild(document.getElementById(data));
+}
+function dragover_handler(ev) {
+ console.log("dragOver");
+ ev.preventDefault();
 }
 function add() {
     var task = document.getElementById('task').value;
+    console.log(task);
+    var newcard = document.createElement("li");
+    var textfield = document.createElement("p");
+    var removebutton = document.createElement("button");
+    removebutton.setAttribute("class","rmbtn");
+    removebutton.innerHTML="X";
+    removebutton.addEventListener("click",remove);
+    newcard.appendChild(textfield);
+    newcard.appendChild(removebutton);
+    textfield.innerHTML= task;
+    newcard.setAttribute("id",counter);
+    counter +=1;
+    newcard.setAttribute("class","card-panel");
+    newcard.setAttribute("draggable","true");
+    newcard.setAttribute("ondrag","drag_handler(event);");
+    newcard.setAttribute("ondragstart","dragstart_handler(event);");
+    var todos=document.getElementById('todos');
+	todos.appendChild(newcard);
+	inputbox=document.getElementById('task');
+	inputbox.value="";
 
-    var todos = get_todos();
-    todos.push(task);
-    localStorage.setItem('todo', JSON.stringify(todos));
-
-    show();
-
-    return false;
-}
-function show() {
-    var todos = get_todos();
-
-    var html = '<ul class="listoftodos">';
-    for(var i=0; i<todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-    };
-    html += '</ul>';
-
-    document.getElementById('todos').innerHTML = html;
-
-    var buttons = document.getElementsByClassName('remove');
-    for (var i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
-    };
 }
 function remove() {
-    var id = this.getAttribute('id');
-    var todos = get_todos();
-    todos.splice(id, 1);
-    localStorage.setItem('todo', JSON.stringify(todos));
-
-    show();
-
-    return false;
+ 	var listtoremove = this.parentNode;
+ 	listtoremove.remove();
 }
-function show() {
-    var todos = get_todos();
- 
-    var html = '<ul class="god" >';
-    for(var i=0; i<todos.length; i++) {
-        html += '<li class="card-panel">' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-    };
-    html += '</ul>';
- 
-    document.getElementById('todos').innerHTML = html;
- 
-    var buttons = document.getElementsByClassName('remove');
-    for (var i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
-    };
-}
- 
 document.getElementById('add').addEventListener('click', add);
-show();
